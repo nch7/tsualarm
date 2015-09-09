61945300a1d31ca167aa844eb7a2942db5b9b324
Nick Chikovani <nikabika97@gmail.com>
2015-09-10 00:37:32 +0400

    added logs folder

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 	$config = [
-		'cookie'		=> '', // ჩააგდეთ თქვენი cookie!
+		'cookie'		=> '__utma=20719549.120572707.1440710570.1441693569.1441748970.4; __utmz=20719549.1440710570.1.1.utmcsr=facebook.com|utmccn=(referral)|utmcmd=referral|utmcct=/; ASPSESSIONIDCQRRQSSA=AFGJFFODAELKEBFJJPGNJEIL; ASP.NET_SessionId=tap003bp2au553a05bah0nr1; ASPSESSIONIDQCSQTTTB=LKBFGMODNCIEKKEJDDACOACE; ASPSESSIONIDQCQTSQRD=DOPIHKPDHEFINKPHJBMHJBPG; ASPSESSIONIDSCQSSTQC=IMAAALAAJFAGDNHNBLFGMOKM; _gat=1; _ga=GA1.2.120572707.1440710570', // ჩააგდეთ თქვენი cookie!
 		'succussText'	=> 'საგნის დამატება',
 		'loggedInText'	=> 'სისტემაში შესულია სტუდენტი'
 	];
